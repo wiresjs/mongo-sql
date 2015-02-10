0393623a00f4afc9f23d5fc1fb864dc8690172d2
Ivan Orlov <nchanged@gmail.com>
2015-02-10 13:09:31 +0200

    Changes to fields

--- helpers/query/if-exists.js
+++ helpers/query/if-exists.js
@@ -5,3 +5,8 @@ var utils   = require('../../lib/utils');
 helpers.register('ifExists', function(ifExists, values, query){
   return ifExists ? 'if exists' : null;
 });
+
+
+helpers.register('exists', function(exists, values, query){
+  return values ? 'is not null' : 'is null';
+});
